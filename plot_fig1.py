ee2aee30fbe0cfae10e483064d29145c546ed0a2
Jasper van den Bosch <japsai@gmail.com>
2024-09-05 12:18:05 +0100

    plot dataset

--- plot_fig1.py
+++ plot_fig1.py
@@ -1,9 +1,30 @@
 """Plot various parts for figure 1
 
 See prep_fig1.py
+
+todo
+
+- rotate
+- axis labels
 """
 from os.path import expanduser, join
+from rsatoolbox.data.dataset import load_dataset
+import seaborn
+import matplotlib.pyplot as plt
 
 
-data_dir = expanduser('~/data/rsatoolbox/mur32/figure1')
-out_dir = join(data_dir, 'derivatives', 'figure1')
+data_dir = expanduser('~/data/rsatoolbox/mur32/derivatives/figure1')
+
+"""
+dataset.h5
+rmds.h5
+noise.npy
+meta.json
+"""
+
+ds = load_dataset(join(data_dir, 'dataset.h5'))
+ds_run0 = ds.split_obs('run')[0]
+plt.figure()
+seaborn.heatmap(ds_run0.measurements[:3, :5], annot=True)
+plt.savefig('dataset.svg')
+plt.close('all')
