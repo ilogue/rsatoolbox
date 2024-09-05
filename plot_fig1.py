446bff1d529d8bc0dc9d045fd0ddce56025f409c
Jasper van den Bosch <japsai@gmail.com>
2024-09-05 16:08:52 +0100

    next step

--- plot_fig1.py
+++ plot_fig1.py
@@ -12,7 +12,11 @@ import numpy
 import seaborn
 import matplotlib.pyplot as plt
 
-n_channels = 3
+n_channels = 5
+data_cmap = 'mako'
+conditions = ['image_bagel', 'image_glove', 'image_kettle',
+              'text_bagel', 'text_glove', 'text_kettle']
+voxel_selection = []
 
 
 data_dir = expanduser('~/data/rsatoolbox/mur32/derivatives/figure1')
@@ -25,10 +29,11 @@ meta.json
 
 ds = load_dataset(join(data_dir, 'dataset.h5'))
 ds_run0 = ds.split_obs('run')[0]
+ds_run0 = ds_run0.subset_obs('condition', conditions)
 plt.figure()
 ax = seaborn.heatmap(
-    ds_run0.measurements[:n_channels, :5].T,
-    annot=True, cbar=False, cmap='bone', vmin=-20, vmax=+20)
+    ds_run0.measurements[:, :n_channels],
+    annot=True, cbar=False, square=True, cmap=data_cmap, vmin=-20, vmax=+20)
 ax.set(xlabel='channels', ylabel='observations')
 ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
 plt.savefig('dataset.svg')
@@ -41,15 +46,16 @@ prec_run0 = prec[0,:,:]
 plt.figure()
 ax = seaborn.heatmap(
     prec_run0[:n_channels, :n_channels],
-    annot=True, cbar=False, cmap='bone', vmin=0, vmax=+0.01)
+    annot=True, cbar=False, square=True, cmap=data_cmap, vmin=0, vmax=+0.025)
 ax.set(xlabel='channels', ylabel='channels')
 ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-plt.savefig('noise.svg')
+plt.savefig('prec.svg')
 plt.close('all')
 
 
 
 rdms = load_rdm(join(data_dir, 'rdms.h5'))
+rdms = rdms.subsample_pattern('condition', conditions)
 plt.figure()
 fig, _, _ = show_rdm(
     rdms,
